aa22fe3b97ff1ee199ae97f27374f5da5a730f44
Hayley Bech <hayleybech@gmail.com>
2026-05-28 21:48:56 +0800

    Add game start/win/restart

--- app/cooow.tsx
+++ app/cooow.tsx
@@ -12,7 +12,7 @@ import {useRouter} from "expo-router";
 
 export type CowBreed = 'holstein-friesian' | 'angus' | 'hereford' | 'highland';
 type ConnectionStatus = 'initial' | 'open' | 'closed' | 'reconnecting';
-type GameNotification = { type: 'paused' } | { type: 'resumed' } | { type: 'changed_direction', payload: Direction } | { type: 'powerup_stored' } | { type: 'powerup_used' } | { type: 'died' };
+type GameNotification = { type: 'paused' } | { type: 'resumed' } | { type: 'started' } | { type: 'changed_direction', payload: Direction } | { type: 'powerup_stored' } | { type: 'powerup_used' } | { type: 'died' } | { type: 'game_over', payload: { winner: string } };
 
 export default function CooowScreen() {
     const props = useContext(ScreenPropsContext);
@@ -22,9 +22,12 @@ export default function CooowScreen() {
     const [connStatus, setConnStatus] = useState<ConnectionStatus>('initial');
     const router = useRouter();
     const [isPaused, setIsPaused] = useState<boolean>(true);
+    const [hasStarted, setHasStarted] = useState<boolean>(false);
     const [currentDirection, setCurrentDirection] = useState<Direction | undefined>(undefined);
     const [hasPowerup, setHasPowerup] = useState<boolean>(false);
     const [isDead, setIsDead] = useState<boolean>(false);
+    const [isGameEnded, setIsGameEnded] = useState<boolean>(false);
+    const [winner, setWinner] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         if (!props.connRef.current) {
@@ -34,11 +37,20 @@ export default function CooowScreen() {
         props.onDataCallbackRef.current = (data: unknown) => {
             const action = data as GameNotification;
 
+            console.log('action', action);
+
             if (action.type === 'paused') {
                 setIsPaused(true);
             }
             if (action.type === 'resumed') {
                 setIsPaused(false);
+            }
+            if (action.type === 'started') {
+                setHasStarted(true);
+                setIsPaused(false);
+                setIsGameEnded(false);
+                setIsDead(false);
+                setWinner(undefined);
             }
             if (action.type === 'changed_direction') {
                 setCurrentDirection(action.payload);
@@ -52,6 +64,10 @@ export default function CooowScreen() {
             if (action.type === 'died') {
                 setIsDead(true);
             }
+            if (action.type === 'game_over') {
+                setIsGameEnded(true);
+                setWinner(action.payload.winner);
+            }
         };
 
         props.connRef.current.on('close', () => {
@@ -62,15 +78,22 @@ export default function CooowScreen() {
 
     }, []);
 
-    const requestPause = useCallback(() => {
+    const requestPauseOrStart = useCallback(() => {
         if (!props.connRef.current) {
             return;
         }
 
+        if (isGameEnded) {
+            props.connRef.current.send({
+                type: 'start_game',
+            })
+            return;
+        }
+
         props.connRef.current.send({
-            type: 'pause',
+            type: hasStarted ? 'pause' : 'start_game',
         })
-    }, [])
+    }, [hasStarted, props.connRef, isGameEnded])
 
     const drop = useCallback(() => {
         if (!props.connRef.current) {
@@ -123,8 +146,8 @@ export default function CooowScreen() {
                                 <Text className="text-neutral-400 text-sm italic">Best played in landscape 🔄</Text>
                             )}
                         </View>
-                        <Button onPress={requestPause} disabled={!props.connRef.current || isDead}>
-                            {isPaused ? 'Resume' : 'Pause'}
+                        <Button onPress={requestPauseOrStart} disabled={!props.connRef.current || (isDead && !isGameEnded)}>
+                            {isGameEnded ? 'Play Again' : (!hasStarted ? 'Start Game' : (isPaused ? 'Resume' : 'Pause'))}
                         </Button>
                     </View>
 
@@ -134,17 +157,27 @@ export default function CooowScreen() {
                         </View>
                     )}
 
+                    {isGameEnded && (
+                        <View className="bg-blue-500 p-4 rounded-lg items-center shadow-lg">
+                            <Text className="text-white font-bold text-2xl mb-2">GAME OVER</Text>
+                            <Text className="text-white text-center font-bold text-xl mb-1">
+                                {winner === props.usernameRef.current ? 'YOU WON 🏆' : `WINNER: ${winner}`}
+                            </Text>
+                            <Text className="text-white text-center">The game has ended. Ready for another round?</Text>
+                        </View>
+                    )}
+
                     <View className="gap-2 grow flex-1">
-                        <Button onPress={usePowerup} disabled={!props.connRef.current || isPaused || !hasPowerup || isDead} className="grow">
+                        <Button onPress={usePowerup} disabled={!props.connRef.current || isPaused || !hasPowerup || isDead || isGameEnded} className="grow">
                             Use
                         </Button>
-                        <Button onPress={drop} disabled={!props.connRef.current || isPaused || !hasPowerup || isDead} className="grow">
+                        <Button onPress={drop} disabled={!props.connRef.current || isPaused || !hasPowerup || isDead || isGameEnded} className="grow">
                             Drop Trap
                         </Button>
                     </View>
                 </View>
 
-                <SwipeArea onSwipe={move} disabled={isPaused || isDead} isDead={isDead}/>
+                <SwipeArea onSwipe={move} disabled={isPaused || isDead || isGameEnded} isDead={isDead || isGameEnded}/>
             </View>
 
         </View>
